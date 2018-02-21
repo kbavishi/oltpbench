727365aef43c0982ce2bf93c7584a82b6da0916f
Karan Bavishi <kbavishi@cs.wisc.edu>
2018-02-20 21:36:52 -0600

    Fix test name in script

--- run_dyn_tests.sh
+++ run_dyn_tests.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-python pred_history_twitter_benchmark.py --rate=10 --alpha=0.5 --gedf_factor=0.4 --iter=3 --fixed_deadline=false --random_page_cost=4.0 127.0.0.1
+python dynamic_history_benchmark.py --rate=10 --alpha=0.5 --gedf_factor=0.4 --iter=3 --fixed_deadline=false --random_page_cost=4.0 127.0.0.1
 
-python pred_history_twitter_benchmark.py --rate=15 --alpha=0.5 --gedf_factor=0.4 --iter=3 --fixed_deadline=true --random_page_cost=4.0 127.0.0.1
+python dynamic_history_benchmark.py --rate=15 --alpha=0.5 --gedf_factor=0.4 --iter=3 --fixed_deadline=true --random_page_cost=4.0 127.0.0.1
 
-python pred_history_twitter_benchmark.py --rate=20 --alpha=0.5 --gedf_factor=0.4 --iter=3 --fixed_deadline=false --random_page_cost=4.0 127.0.0.1
+python dynamic_history_benchmark.py --rate=20 --alpha=0.5 --gedf_factor=0.4 --iter=3 --fixed_deadline=false --random_page_cost=4.0 127.0.0.1
