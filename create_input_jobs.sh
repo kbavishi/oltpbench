3d2e7c6b679a207428da1e25b8c1d50a2f2bb8d8
Karan Bavishi <kbavishi@cs.wisc.edu>
2018-03-07 11:51:45 -0600

    Add random job trace extraction

--- create_input_jobs.sh
+++ create_input_jobs.sh
@@ -11,6 +11,17 @@ sed -i 's/(.*.java:.*) INFO  - JOB GetUserTweets: /4,/' /tmp/jobs.txt
 sed -i 's/(.*.java:.*) INFO  - JOB InsertTweet: /5,/' /tmp/jobs.txt
 mv /tmp/jobs.txt ~/input_jobs.txt
 
+echo "Sanitizing random jobs trace file"
+grep JOB random_jobs_trace.txt > /tmp/jobs.txt
+cp /tmp/jobs.txt ~/random_jobs.txt
+cut -f1 -d " " --complement ~/random_jobs.txt > /tmp/jobs.txt
+sed -i 's/(.*.java:.*) INFO  - JOB GetTweet: /1,/' /tmp/jobs.txt
+sed -i 's/(.*.java:.*) INFO  - JOB GetTweetsFromFollowing: /2,/' /tmp/jobs.txt
+sed -i 's/(.*.java:.*) INFO  - JOB GetFollowers: /3,/' /tmp/jobs.txt
+sed -i 's/(.*.java:.*) INFO  - JOB GetUserTweets: /4,/' /tmp/jobs.txt
+sed -i 's/(.*.java:.*) INFO  - JOB InsertTweet: /5,/' /tmp/jobs.txt
+mv /tmp/jobs.txt ~/random_jobs.txt
+
 echo "Fetching new input job costs from PostgresSQL database"
 python new_input_jobs_costs.py $1 $2 $3 true > ~/input_jobs_loc_cost_new.txt
 
